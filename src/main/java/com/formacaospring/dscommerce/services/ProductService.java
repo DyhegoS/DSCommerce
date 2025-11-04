f8c819054277e10f9d0ba2e65eef736cb3c13297
DyhegoS <dyhegodasilva@gmail.com>
2025-11-04 12:21:51 -0300

    criado ProductMinDto

--- src/main/java/com/formacaospring/dscommerce/services/ProductService.java
+++ src/main/java/com/formacaospring/dscommerce/services/ProductService.java
@@ -15,6 +15,7 @@ import com.formacaospring.dscommerce.services.exceptions.ResourceNotFoundExcepti
 import jakarta.persistence.EntityNotFoundException;
 
 import com.formacaospring.dscommerce.dto.ProductDTO;
+import com.formacaospring.dscommerce.dto.ProductMinDTO;
 import com.formacaospring.dscommerce.entities.Product;
 
 @Service
@@ -31,9 +32,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable) {
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
         Page<Product> result = repository.searchByName(name, pageable);
-        return result.map(x -> new ProductDTO(x));
+        return result.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
