88c56644b90cd98d9839987554e30cc2db0afa70
DyhegoS <dyhegodasilva@gmail.com>
2025-11-06 15:58:20 -0300

    implementado busca por categorias e ajustes nas validações de campos

---
+++ src/main/java/com/formacaospring/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.formacaospring.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.formacaospring.dscommerce.dto.CategoryDTO;
+import com.formacaospring.dscommerce.services.CategoryService;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
---
+++ src/main/java/com/formacaospring/dscommerce/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.formacaospring.dscommerce.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.formacaospring.dscommerce.repositories.CategoryRepository;
+import com.formacaospring.dscommerce.dto.CategoryDTO;
+import com.formacaospring.dscommerce.entities.Category;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+}
