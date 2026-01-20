70266b9c5f023e4e7ea7b0acc30922d40f1f73a9
DyhegoS <dyhegodasilva@gmail.com>
2026-01-20 18:38:33 -0300

    teste de order

--- src/test/java/com/formacaospring/dscommerce/controllers/OrderControllerIT.java
+++ src/test/java/com/formacaospring/dscommerce/controllers/OrderControllerIT.java
@@ -4,6 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.Instant;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.formacaospring.dscommerce.dto.OrderDTO;
 import com.formacaospring.dscommerce.entities.Order;
+import com.formacaospring.dscommerce.entities.OrderItem;
+import com.formacaospring.dscommerce.entities.OrderStatus;
+import com.formacaospring.dscommerce.entities.Product;
 import com.formacaospring.dscommerce.entities.User;
 import com.formacaospring.dscommerce.tests.OrderFactory;
+import com.formacaospring.dscommerce.tests.ProductFactory;
 import com.formacaospring.dscommerce.tests.TokenUtil;
 import com.formacaospring.dscommerce.tests.UserFactory;
 
@@ -32,40 +39,44 @@ public class OrderControllerIT {
 	@Autowired
 	private TokenUtil tokenUtil;
 	
-	long existingOrderId, nonExistingOrderId;
+	private Long existingOrderId, nonExistingOrderId;
 	private String usernameAdmin, usernameClient, passwordAdmin, passwordClient, tokenAdmin, tokenClient, invalidToken;
 	private Order order;
-	private User admin, client, otherClient;
+	private OrderDTO orderDTO;
+	private User user;
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		existingOrderId = 1L;
 		nonExistingOrderId = 1000L;
+		
 		usernameAdmin = "alex@gmail.com";
 		passwordAdmin = "123456";
 		usernameClient = "maria@gmail.com";
 		passwordClient = "123456";
-		admin = UserFactory.createAdminUser();
-		client = UserFactory.createClientUser();
-		otherClient = UserFactory.createCustomClientUser(2L, "pangare@gmail.com");
+		
+		user = UserFactory.createClientUser();
+		order = new Order(null, Instant.now(), OrderStatus.WAITING_PAYMENT, user, null);
+		
+		Product product = ProductFactory.createProduct();
+		OrderItem orderItem = new OrderItem(order, product, 2, 10.0);
+		order.getItems().add(orderItem);
+		
 		tokenAdmin = tokenUtil.obtainAccessToken(mockMvc, usernameAdmin, passwordAdmin);
 		tokenClient = tokenUtil.obtainAccessToken(mockMvc, usernameClient, passwordClient);
 		invalidToken = tokenAdmin + "xpto";		
 	}
 	
 	@Test
-	public void findByIdShouldReturnOrderDTOWhenAdminLogged() throws Exception{
-		Order orderTest = new Order();
-		orderTest = OrderFactory.createOrder(admin);
+	public void findByIdShouldReturnOrderDTOWhenIdExistsAndAdminLogged() throws Exception{
 		
 		ResultActions result =
-                mockMvc.perform(get("/orders/{id}", orderTest.getId())
+                mockMvc.perform(get("/orders/{id}", existingOrderId)
                         .header("Authorization", "Bearer " + tokenAdmin)
-                        .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isOk());
-        result.andExpect(jsonPath("$.id").value(orderTest.getId()));
-        result.andExpect(jsonPath("$.moment").value(orderTest.getMoment()));
+        result.andExpect(jsonPath("$.id").value(existingOrderId));
+        result.andExpect(jsonPath("$.moment").value("2022-07-25T13:00:00Z"));
 	}
 	
 	@Test
