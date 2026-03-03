4d6086b8e1cdd62fd3d56c2689087f75ba123b24
DyhegoS <dyhegodasilva@gmail.com>
2026-03-02 23:08:00 -0300

    update: login screen

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { BaseUi } from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPage],
+  imports: [RouterOutlet, LoginPage, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
