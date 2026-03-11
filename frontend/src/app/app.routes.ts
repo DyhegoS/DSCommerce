73cd9466fc0db81e23a3c7d5cad9874948f6e70e
DyhegoS <dyhegodasilva@gmail.com>
2026-03-11 18:00:07 -0300

    UPDATE: ajusts in text colors

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Home } from './_components/home/home';
 import { UsersComponent } from './_components/users/users';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'home', component: Home },
